10656bf668dc8270166dce3841b24fe5f45b9f09
Priyank Kumar <localhostservers@gmail.com>
2024-01-23 20:39:45 +0530

    check gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\priya\OneDrive\Desktop\geminillmapp\venv
+set VIRTUAL_ENV=C:\Users\priya\OneDrive\Desktop\streamlit-gemini\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
